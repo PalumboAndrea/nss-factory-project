6ff034885f1cf93d35aba25c73c6a8b0241e5d09
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-29 01:18:47 +0200

    implement responsiveness

--- src/components/Main/MainComponents/CarouselComponent.vue
+++ src/components/Main/MainComponents/CarouselComponent.vue
@@ -43,15 +43,17 @@ export default {
 <template>
     <div class="container-fluid p-0 carouselContainer d-flex align-items-center position-relative">
         <div class="carouselText d-flex flex-column m-auto text-center align-items-center pt-5">
-            <img :src="getImagePath(content[actualIndex].img)" alt="" id="mainImg">
+            <img :src="getImagePath(content[actualIndex].img)" alt="" id="mainImg" class="img-fluid">
             <p class="text-uppercase mb-5 uppercaseTitle">
                 london collection season
             </p>
-            <p class="mainTitle mt-3 mb-3">
-                {{ content[actualIndex].upperTitle }}
-                <br>
-                {{ content[actualIndex].lowerTitle }}
-            </p>
+            <transition name="slide-fade" mode="out-in">
+                <p class="mainTitle mt-3 mb-3" :key="content[actualIndex].upperTitle">
+                    {{ content[actualIndex].upperTitle }}
+                    <br>
+                    {{ content[actualIndex].lowerTitle }}
+                </p>
+            </transition>
             <p class="subtitle">
                 An estimable experience in the modern collection house
             </p>
@@ -62,8 +64,8 @@ export default {
             </div>
         </div>
 
-        <div class="position-absolute previewNextSlide" @click="carouselIndex">
-            <img :src="getImagePath(content[otherIndex].img)" alt="" id="previewImg">
+        <div class="previewNextSlide" @click="carouselIndex">
+            <img :src="getImagePath(content[otherIndex].img)" alt="" class="img-fluid previewImg">
             <div class="previewNextSlideContent">
                 <p class="previewNextSlideTitle">
                     {{ content[otherIndex].upperTitle }}
@@ -80,12 +82,40 @@ export default {
                 </div>
             </div>
         </div>
+        <!-- this is needed for down lg breakpoint -->
+        <div class="text-uppercase position-absolute nextSM m-0" @click="carouselIndex">
+            <span>next</span>
+            <div class="iconContainer d-flex justify-content-center align-items-center position-absolute">
+                <font-awesome-icon :icon="['fas', 'chevron-right']" class="icon"/>
+                <font-awesome-icon :icon="['fas', 'chevron-right']" class="icon iconNone"/>
+                <font-awesome-icon :icon="['fas', 'chevron-right']" class="icon iconNone"/>
+            </div>
+        </div>
     </div>
     
 </template>
 
 <style lang="scss" scoped>
 @use "../../../styles/general.scss" as *;
+
+.slide-fade-enter-active {
+  transition: all 0.2s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.2s ease-in;
+}
+
+.slide-fade-enter-from{
+  transform: translateX(-10px);
+  opacity: 0;
+}
+.slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+
 
 .carouselContainer{
     height: calc(100vh - $headerHeight);
@@ -101,14 +131,58 @@ export default {
         object-fit: cover;
     }
 
+    .nextSM{
+        display: none;
+        font-family: $uppercaseTitleFont;
+        font-weight: 700;
+        letter-spacing: 3px;
+        cursor: pointer;
+        &:hover .iconNone{
+            opacity: 1;
+        }
+        @include media-breakpoint-down(lg) {
+            display: flex;
+            bottom: 10px;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    }
+    .iconContainer{
+        left: 75px;
+        bottom: 0;
+        transform: translate(0, -50%);
+        .icon{
+            font-size: 0.8rem;
+            margin-right: 2px;
+        }
+        .iconNone{
+            opacity: 0;
+            transition: opacity 0.3s ease-out;
+        }
+    }
+
     .previewNextSlide{
+        position: absolute;
         right: 70px;
         bottom: 0;
         height: 300px;
         width: 440px;
+        transform: scale(0.8);
+        transform-origin: bottom center;
         transition: 0.3s;
         cursor: pointer;
         overflow: hidden;
+        @include media-breakpoint-down(xxl) {
+            transform: scale(0.7);
+            transform-origin: bottom right;
+        }
+        @include media-breakpoint-down(xl) {
+            transform: scale(0.6);
+            transform-origin: bottom right;
+        }
+        @include media-breakpoint-down(lg) {
+            display: none;
+        }
         .next{
             font-family: $uppercaseTitleFont;
             font-weight: 700;
@@ -116,37 +190,26 @@ export default {
             right: 80px;
             letter-spacing: 3px;
         }
-
         .iconContainer{
-                left: 75px;
-                bottom: 0;
-                transform: translate(0, -50%);
-                .icon{
-                    font-size: 0.8rem;
-                    margin-right: 2px;
-                }
-                .iconNone{
-                    opacity: 0;
-                    transition: opacity 0.3s ease-out;
-                }
+            left: 75px;
+            bottom: 0;
+            transform: translate(0, -50%);
+            .icon{
+                font-size: 0.8rem;
+                margin-right: 2px;
             }
-        
+            .iconNone{
+                opacity: 0;
+                transition: opacity 0.3s ease-out;
+            }
+        }
         .previewNextSlideTitle{
             font-size: 22px;
             transition: 0.3s;
             margin: 40px;
             font-family: $titleFont;
         }
-        &:hover .previewNextSlideTitle{
-            font-size: 30px;
-        }
-        &:hover #previewImg{
-            transform: scale(1.2);
-        }
-        &:hover .iconNone{
-            opacity: 1;
-        }
-        #previewImg{
+        .previewImg{
             filter: brightness(0.8);
             padding: 0;
             position: absolute;
@@ -156,6 +219,15 @@ export default {
             z-index: -1;
             object-fit: cover;
             transition: 0.3s all ease-in-out;
+        }
+        &:hover .previewImg{
+            transform: scale(1.2);
+        }
+        &:hover .previewNextSlideTitle{
+            font-size: 30px;
+        }
+        &:hover .iconNone{
+            opacity: 1;
         }
     }
 
