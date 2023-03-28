8e5d0beef7607075a23c0c8b8d876f913e6c9178
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-28 21:09:48 +0200

    add the css and dyamic carousel

--- src/components/Main/MainComponents/WatchLabStudioComponent.vue
+++ src/components/Main/MainComponents/WatchLabStudioComponent.vue
@@ -7,59 +7,95 @@ export default {
     },
     data(){
         return{
+            actualIndex: 0,
+            images: [
+                {
+                    date: '18 settembre 2022',
+                    title: 'Most Important Days on Watchlab',
+                    name: 'important_days.png',
+                },
+                {
+                    date: '23 settembre 2022',
+                    title: 'Winner Of Best Manifacture',
+                    name: 'best_manifacture.png',
+                }
+            ]
         }
     },
+    methods: {
+    carouselIndexBack(){
+        if (this.actualIndex == 0){
+            this.actualIndex = (this.images.length - 1);
+        } else {
+            this.actualIndex--
+        }
+    },
+    carouselIndexForward(){
+        if (this.actualIndex == (this.images.length - 1)){
+            this.actualIndex = 0
+        } else {
+            this.actualIndex++
+        }
+    },
+    getImagePath: function (imgPath){
+        return new URL(`/src/assets/imgs/${imgPath}`, import.meta.url).href;
+    }
+  }
 }
 
 </script>
 
 <template>
-    <div class="container mainContainer my-5">
-        <div class="row">
-            <div class="col-4">
-                <h2 class="fs-1">
-                    Press from Watch Lab Studio
-                </h2>
-                
+    <div class="container-fluid main-container mainContainer my-5 position-relative">
+        <div class="row g-0">
+            <div class="col-6">
+                <p class="mainTitle mb-0">
+                    Press from
+                    <br>
+                    Watch Lab Studio
+                </p>
             </div>
         </div>
-        <div class="row">
+        <div class="row g-0">
             <div class="col-4">
-                <p class="textGray mt-4">
+                <p class="mt-5 text w-75">
                     On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain trouble
                 </p>
             </div>
-            <div class="col-5 image">
-
+            <div class="col-5 image pb-4">
+                <img :src="getImagePath(images[actualIndex].name)" alt="image" class="img-fluid">
             </div>
             <div class="col-3 ps-5">
                 <span class="text-uppercase fw-bold">
                     press
                 </span>
-                <p class="text-uppercase mb-5">
-                    18 settembre 2015
+                <p class="text-uppercase mb-5 date">
+                    {{images[actualIndex].date}}
                 </p>
-                <h4>
-                    Most Important Days on Watchlab
-                </h4>
+                <p class="textTitle fs-3">
+                    {{images[actualIndex].title}}
+                </p>
                 <div class="iconContainer mt-5">
-                    <font-awesome-icon :icon="['fas', 'chevron-left']" class="me-5"/>
-                    <font-awesome-icon :icon="['fas', 'chevron-right']" />
+                    <font-awesome-icon :icon="['fas', 'chevron-left']" role="button" class="me-5" @click="carouselIndexBack"/>
+                    <font-awesome-icon :icon="['fas', 'chevron-right']" role="button" @click="carouselIndexForward" />
                 </div>
+                <div class="greyBackground position-absolute"></div>
             </div>
         </div>
-
     </div>
-
 </template>
 
 <style lang="scss" scoped>
 @use "../../../styles/general.scss" as *;
-
-    .image{
-        background-color: blue;
-        height: 400px;
+    .date{
+        color: $mainColor;
     }
-
-
+    .greyBackground{
+        background-color: rgb(217,217,217);
+        height: 100%;
+        width: 45%;
+        right: 70px;
+        top: 0;
+        z-index: -1;
+    }
 </style>
